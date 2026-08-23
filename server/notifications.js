c6062a31c26daadd28e013bf624bb9b876de22f0
msagbini <78831798+msagbini@users.noreply.github.com>
2026-08-23 10:44:46 +0000

    Agregar notifications.js placeholder para evitar error

---
+++ server/notifications.js
@@ -0,0 +1,15 @@
+// server/notifications.js - Placeholder para evitar errores
+export function sendOwnerPush(...args) {
+  console.log('[notifications] sendOwnerPush called with:', args);
+  return Promise.resolve();
+}
+
+export function sendConfirmationEmail(...args) {
+  console.log('[notifications] sendConfirmationEmail called with:', args);
+  return Promise.resolve();
+}
+
+export function sendExtraChargePaidConfirmation(...args) {
+  console.log('[notifications] sendExtraChargePaidConfirmation called with:', args);
+  return Promise.resolve();
+}
